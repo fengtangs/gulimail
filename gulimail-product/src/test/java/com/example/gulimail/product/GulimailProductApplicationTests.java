6151b90c20687e695a8f0c4478c9780b6d6a427f
fengtangs <1554117196@qq.com>
2023-06-10 16:26:51 +0800

    Initial commit

---
+++ gulimail-product/src/test/java/com/example/gulimail/product/GulimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimaill-ware/src/main/java/com/example/gulimaill/ware/GulimaillWareApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulimaill.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimaillWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimaillWareApplication.class, args);
+    }
+
+}
